ed88138b77d8071d9dcc7d01b9648fb23f2ae249
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-12-12 19:18:29 +0200

    feat: modal toggle js
    - add script for modal window open close
    - add transition smooth
    - update html

---
+++ genius-homework-3/js/modal.js
@@ -0,0 +1,10 @@
+const modalOpenBtn = document.querySelector('.modal-btn-open');
+const modalCloseBtn = document.querySelector('.modal-btn-close');
+const backdrop = document.querySelector('.backdrop');
+
+const modalToggle = function () {
+  backdrop.classList.toggle('is-hidden');
+};
+
+modalOpenBtn.addEventListener('click', modalToggle);
+modalCloseBtn.addEventListener('click', modalToggle);
