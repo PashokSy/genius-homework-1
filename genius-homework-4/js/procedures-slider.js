2a1763dca40c954e6cb3fc04d80c3eab303da8de
Pavlo Syzonenko <pavlo.syzonenko@gmail.com>
2023-12-14 14:43:27 +0200

    feat: homework 4
    initial commit

---
+++ genius-homework-4/js/procedures-slider.js
@@ -0,0 +1,23 @@
+let swiper = new Swiper('.swiper-container', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 16,
+  breakpoints: {
+    480: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+  },
+  pagination: {
+    el: '.pagination',
+    bulletClass: 'pagination-button',
+    bulletActiveClass: 'pagination-button-active',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.carousel-button.next',
+    prevEl: '.carousel-button.prev',
+  },
+});
